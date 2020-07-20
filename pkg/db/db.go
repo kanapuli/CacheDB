e04c1eee8983e81a4ba7ba8348605d1b0f08a364
kanapuli <athavankanapuli@gmail.com>
2020-07-20 09:08:02 +0530

    Add NewCacheDB function
    NewCacheDB opens a persistent file to read the Old Key Value pairs
    If old KVs exists, those values are loaded into NewCacheDB else
    an empty CacheDB is returned

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,13 +1,43 @@
 package server
 
-import "sync"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"sync"
+)
 
+//CacheDB contains a map of string to string to store the incoming items
 type CacheDB struct {
 	items map[string]string
 	mu    sync.RWMutex
 }
 
+const (
+	persistentFile = "cachedb.json"
+)
+
 //NewCacheDB creates new cacheDB instance and returns them
 func NewCacheDB() *CacheDB {
+	// Try to open persistent file
+	file, err := os.Open(persistentFile)
+	if err != nil {
+		fmt.Printf("Unable to open the persistent file %v", persistentFile)
+		return &CacheDB{
+			items: make(map[string]string),
+		}
+	}
 
+	oldKVPairs := make(map[string]string)
+	err = json.NewDecoder(file).Decode(&oldKVPairs)
+	if err != nil {
+		fmt.Printf("Failed to decode the persistent file %v", persistentFile)
+		return &CacheDB{
+			items: make(map[string]string),
+		}
+	}
+
+	return &CacheDB{
+		items: oldKVPairs,
+	}
 }
