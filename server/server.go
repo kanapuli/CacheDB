4e8385b9fe444557229edecbaaa208dbd54c5da5
kanapuli <athavankanapuli@gmail.com>
2020-07-20 08:39:34 +0530

    Add Server struct in package server
    Server struct forms the core of cacheDB
    It contains the net.Listener, channels to handle Sigterms and
    connection timeout values

--- server/server.go
+++ server/server.go
@@ -1,5 +1,20 @@
 package server
 
+import (
+	"net"
+	"time"
+)
+
+//Server is the core CacheDb struct
+//Server has the tcp listener, channels to handle Signals and map of client connections
+type Server struct {
+	listener    *net.Listener
+	exit        chan struct{}
+	quit        chan struct{}
+	connections map[int]*net.Conn
+	timeout     time.Duration
+}
+
 //NewServer creates a new DB server and returns that instance to main func
 func NewServer() interface{} {
 	return nil
