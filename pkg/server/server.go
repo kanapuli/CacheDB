a52d775137beaf158e18c920e8a71313e379f3fe
kanapuli <athavankanapuli@gmail.com>
2020-07-21 07:55:41 +0530

    Embed CacheDB into Server struct

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -8,11 +8,12 @@ import (
 //Server is the core CacheDb struct
 //Server has the tcp listener, channels to handle Signals and map of client connections
 type Server struct {
-	listener    *net.Listener
+	listener    net.Listener
 	exit        chan struct{}
 	quit        chan struct{}
-	connections map[int]*net.Conn
+	connections map[int]net.Conn
 	timeout     time.Duration
+	db          CacheDB
 }
 
 //NewServer creates a new DB server and returns that instance to main func
