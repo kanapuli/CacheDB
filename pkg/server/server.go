d401dafcc595b0d71cda0f21b4c5941a8c4dbeb4
kanapuli <athavankanapuli@gmail.com>
2020-07-21 08:02:34 +0530

    Implement NewServer() for server package
    NewServer() returns server struct that contains a net.listener,
    channels to handle Interrupts and timeouts for connection

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,8 +1,16 @@
 package server
 
 import (
+	"log"
 	"net"
 	"time"
+
+	"github.com/kanapuli/CacheDB/pkg/db"
+)
+
+const (
+	port        = "9097"
+	connTimeout = 10
 )
 
 //Server is the core CacheDb struct
@@ -11,14 +19,26 @@ type Server struct {
 	listener    net.Listener
 	exit        chan struct{}
 	quit        chan struct{}
+	cacheDB     db.CacheDB
 	connections map[int]net.Conn
 	timeout     time.Duration
-	db          CacheDB
 }
 
 //NewServer creates a new DB server and returns that instance to main func
 func NewServer() *Server {
-	return nil
+	l, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to create listener on port %v: %v", port, err)
+	}
+	srv := &Server{
+		listener:    l,
+		exit:        make(chan struct{}),
+		quit:        make(chan struct{}),
+		cacheDB:     db.NewCacheDB(),
+		connections: make(map[int]net.Conn),
+		timeout:     connTimeout * time.Second,
+	}
+	return srv
 }
 
 //Stop stops the CacheDB server
